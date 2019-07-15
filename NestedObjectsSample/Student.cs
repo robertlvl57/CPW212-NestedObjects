afba6b4b5b645fbc2fae2f36c50117f07c777df8
robert lowry <lowryrob2001@gmail.com>
2019-07-15 13:44:06 -0700

    Created the student class

---
+++ NestedObjectsSample/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjectsSample
+{
+    class Student
+    {
+        public string FulllName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string HomeAddress { get; set; }
+
+        /// <summary>
+        /// The students Home phone number
+        /// </summary>
+        public string Phone { get; set; }
+    }
+}
