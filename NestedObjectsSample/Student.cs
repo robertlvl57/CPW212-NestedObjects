f92cf8c17a4e68a9b7869cb92d7cd2998d5bd618
robert lowry <lowryrob2001@gmail.com>
2019-07-15 14:34:05 -0700

    Made full name required and validated student email address

--- NestedObjectsSample/Student.cs
+++ NestedObjectsSample/Student.cs
@@ -11,16 +11,50 @@ namespace NestedObjectsSample
     /// </summary>
     class Student
     {
+        private string _fullName;
+        private string _emailAddress;
+
+        public Student(string fullName)
+        {
+            FullName = fullName;
+        }
+
         /// <summary>
         /// The legal first and last
         /// ex. J Doe
         /// </summary>
-        public string FulllName { get; set; }
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("FullName cannot be null or whitespace");
+                }
+                _fullName = value;
+            }
+        }
 
         /// <summary>
         /// The students school provided email address
         /// </summary>
-        public string EmailAddress { get; set; }
+        public string EmailAddress
+        {
+            get { return _emailAddress; }
+            set
+            {
+                //Maybe use REGEX for more strict validation
+                if (value.EndsWith("@student.cptc.edu"))
+                {
+                    _emailAddress = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Email must end with @student.cptc.edu");
+                }
+            }
+        }
 
         /// <summary>
         /// Students mailing address
