65f94375f970fcce648504251ee53091b817d28b
robert lowry <lowryrob2001@gmail.com>
2019-07-17 13:28:40 -0700

    Added Instructor class

---
+++ NestedObjectsSample/Instructor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjectsSample
+{
+    /// <summary>
+    /// An individual instructor
+    /// </summary>
+    class Instructor
+    {
+        public Instructor()
+        {
+            CourseLoad = new List<Course>();
+        }
+
+        /// <summary>
+        /// The official @cptc.edu work email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The location of the office in building/room format
+        /// ex. B11R112
+        /// </summary>
+        public string OfficeLocatioin { get; set; }
+
+        /// <summary>
+        /// The 4 digit extension
+        /// </summary>
+        public string OfficePhone { get; set; }
+
+        /// <summary>
+        /// The first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The courses the instructor is currently teaching
+        /// </summary>
+        public List<Course> CourseLoad { get; set; }
+
+        public void AddCourse(Course c)
+        {
+            if(CourseLoad == null)
+            {
+                CourseLoad = new List<Course>();
+            }
+            CourseLoad.Add(c);
+        }
+    }
+}
